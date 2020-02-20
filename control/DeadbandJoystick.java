bfd5d431298a452282642e98fad0b8822b17e186
Johnny Wobble <46848052+JohnnyWobble@users.noreply.github.com>
2020-02-20 17:12:11 -0500

    More joystick commands

--- control/DeadbandJoystick.java
+++ control/DeadbandJoystick.java
@@ -22,14 +22,32 @@ public class DeadbandJoystick extends Joystick {
     }
 
     public double curveY() {
-        return 1 / this.deadbandY();
+        double y = this.deadbandY();
+        return Math.copySign(y * y, y);
     }
 
     public double curveX() {
-        return 1 / this.deadbandX();
+        double x = this.deadbandX();
+        return Math.copySign(x * x, x);
     }
 
     public double curveZ() {
-        return 1 / this.deadbandZ();
+        double z = this.deadbandZ();
+        return Math.copySign(z * z, z);
+    }
+
+    public double curveYnd() {
+        double y = this.getY();
+        return Math.copySign(y * y, y);
+    }
+
+    public double curveXnd() {
+        double x = this.getX();
+        return Math.copySign(x * x, x);
+    }
+
+    public double curveZnd() {
+        double z = this.getZ();
+        return Math.copySign(z * z, z);
     }
 }
