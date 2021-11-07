4dccfa139cc30edd0b110f85ae67215091e130b3
Ben B <lerminator3@gmail.com>
2021-11-07 01:27:03 -0400

    created a base drivetrain
    Created a base drivetrain with arcade, tank, tankvoltage, and reverse tankvoltage methods.

--- subsystems/DrivetrainBase.java
+++ subsystems/DrivetrainBase.java
@@ -1,5 +1,56 @@
 package org.frc5587.lib.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+
 public class DrivetrainBase {
-    
+    // create leader and follower motors for the drivetrain
+    private final WPI_TalonFX leftLeader = new WPI_TalonFX(0);
+    private final WPI_TalonFX rightLeader = new WPI_TalonFX(1);
+    private final WPI_TalonFX leftFollower = new WPI_TalonFX(2);
+    private final WPI_TalonFX rightFollower = new WPI_TalonFX(3);
+
+    // group the leader and follower motors so they can be controlled at the same time
+    private final SpeedControllerGroup leftGroup = new SpeedControllerGroup(leftLeader, leftFollower);
+    private final SpeedControllerGroup rightGroup = new SpeedControllerGroup(rightLeader, rightFollower);
+
+    // make the speed controller groups into one drivetrain object
+    private final DifferentialDrive differentialDrive = new DifferentialDrive(rightGroup, leftGroup);
+
+    // sets the motor modes and their speeds to 0
+    public void startTalons() {
+        leftLeader.set(ControlMode.PercentOutput, 0);
+        rightLeader.set(ControlMode.PercentOutput, 0);
+        leftFollower.set(ControlMode.Follower, 0);
+        rightFollower.set(ControlMode.Follower, 1);
+    }
+
+    // drive with a given throttle and curve (arcade drive)
+    public void arcadeDrive(double throttle, double curve) {
+        differentialDrive.arcadeDrive(throttle, curve, false);
+    }
+
+    // drive with a given throttle for each side of the robot (tank drive)
+    public void tankDrive(double leftThrottle, double rightThrottle) {
+        differentialDrive.tankDrive(leftThrottle, rightThrottle, false);
+    }
+
+    // a tank drive that sets the voltages of the motors
+    public void tankDriveVolts(double leftVolts, double rightVolts) {
+        leftGroup.setVoltage(-leftVolts);
+        rightGroup.setVoltage(rightVolts);
+        differentialDrive.feed();
+    }
+
+    // a reversed version of tankdrivevolts
+    public void tankDriveVoltsReverse(double leftVolts, double rightVolts) {
+        this.tankDriveVolts(-leftVolts, -rightVolts);
+    }
+
+    // stops all motors
+    public void stop() {
+        differentialDrive.stopMotor();
+    }
 }
