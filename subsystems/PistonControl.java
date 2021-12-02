82025b994d94d6321290d402e5b7f6fcd47a7389
Andrew Lawlor <lawlorcube06@gmail.com>
2021-12-02 16:23:39 -0500

    document new java-libs code

--- subsystems/PistonControl.java
+++ subsystems/PistonControl.java
@@ -4,27 +4,25 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class PistonControl extends SubsystemBase{
+public abstract class PistonControl extends SubsystemBase{
     private DoubleSolenoid[] doubleSolenoidPistons;
 
     public PistonControl(int[] pistonIDS) {
-        for(int i = 0; i < pistonIDS.length; i+=2) {
-            doubleSolenoidPistons[i] = new DoubleSolenoid(i, i+1);
+        for(int i = 0; i < pistonIDS.length; i+=2) {  // for the amount of numbers in the pistonIDS array defined in constants
+            doubleSolenoidPistons[i] = new DoubleSolenoid(i, i+1);  // create a new double solenoid
         }
 
     }
 
-    public void extend() {
-        for(DoubleSolenoid piston : doubleSolenoidPistons) {
+    public void extend() {  // method to extend the piston(s)
+        for(DoubleSolenoid piston : doubleSolenoidPistons) { // extends the piston for every piston created
             piston.set(kForward);
         }
     }
 
-    public void retract() {
-        for(DoubleSolenoid piston : doubleSolenoidPistons) {
+    public void retract() {  // method to retract the piston(s)
+        for(DoubleSolenoid piston : doubleSolenoidPistons) { // retracts the piston for every piston created
             piston.set(kReverse);
         }
     }
 }
-
-// hazel did this stuff
