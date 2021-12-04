c0cb893cba09a78d8df336b81964ec8832de7f5c
Andrew Lawlor <lawlorcube06@gmail.com>
2021-12-04 10:43:06 -0500

    restructured pistoncontrol to use 2d arrays, added more specific methods

--- subsystems/PistonControl.java
+++ subsystems/PistonControl.java
@@ -5,18 +5,32 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import static edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class PistonControl extends SubsystemBase{
+public abstract class PistonControl extends SubsystemBase{
     private DoubleSolenoid[] doubleSolenoidPistons;
 
-    public PistonControl(int[] pistonIDS) {
-        for(int i = 0; i < pistonIDS.length; i+=2) {
-            doubleSolenoidPistons[i] = new DoubleSolenoid(i, i+1);
+    public PistonControl(int[][] pistonIDS) {
+        doubleSolenoidPistons = new DoubleSolenoid[pistonIDS.length/2];
+
+        for(int i = 0; i < pistonIDS.length; i++) {
+            doubleSolenoidPistons[i] = new DoubleSolenoid(pistonIDS[i][0], pistonIDS[i][1]);
         }
     }
 
     public void pistonsSet(Value direction) {
         for(DoubleSolenoid piston : doubleSolenoidPistons) {
             piston.set(direction);
-        }        
+        }
+    }
+
+    public void extend() {
+        for(DoubleSolenoid piston : doubleSolenoidPistons) {
+            piston.set(Value.kForward);
+        }
+    }
+
+    public void retract() {
+        for(DoubleSolenoid piston : doubleSolenoidPistons) {
+            piston.set(Value.kReverse);
+        }
     }
 }
