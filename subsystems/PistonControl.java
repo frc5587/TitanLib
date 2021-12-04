bde631f765e17cc5f65af9a779dddc38eed37b1d
csokolove <94080789+csokolove@users.noreply.github.com>
2021-12-03 20:25:48 -0500

    Abstract class, outline 2d array structure

--- subsystems/PistonControl.java
+++ subsystems/PistonControl.java
@@ -5,15 +5,21 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import static edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class PistonControl extends SubsystemBase{
+public abstract class PistonControl extends SubsystemBase{
     private DoubleSolenoid[] doubleSolenoidPistons;
 
     public PistonControl(int[] pistonIDS) {
+        doubleSolenoidPistons = new DoubleSolenoid[pistonIDS.length/2];
+        int[][] a= {
+            {}
+        };
+        // TODO Create 2D array to rid of i+=2
         for(int i = 0; i < pistonIDS.length; i+=2) {
-            doubleSolenoidPistons[i] = new DoubleSolenoid(i, i+1);
+            doubleSolenoidPistons[i] = new DoubleSolenoid(pistonIDS[i], pistonIDs[i + 1]);
         }
     }
 
+    // TODO Include extend() and retract() methods as an alternative
     public void pistonsSet(Value direction) {
         for(DoubleSolenoid piston : doubleSolenoidPistons) {
             piston.set(direction);
