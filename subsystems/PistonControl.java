d9ba95be5e1c8f871ab0bb4667feef664dce2fb0
Andrew Lawlor <lawlorcube06@gmail.com>
2021-12-06 22:03:47 -0500

    added more specific method calls, more documentation

--- subsystems/PistonControl.java
+++ subsystems/PistonControl.java
@@ -5,34 +5,38 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import static edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public abstract class PistonControl extends SubsystemBase{
+public abstract class PistonControl extends SubsystemBase {
     private DoubleSolenoid[] doubleSolenoidPistons;
 
+    /**
+     * Creates new doublesolenoids for the amount of piston ports specified in
+     * constants.java
+     */
     public PistonControl(int[][] pistonIDS) {
-        doubleSolenoidPistons = new DoubleSolenoid[pistonIDS.length/2];
+        doubleSolenoidPistons = new DoubleSolenoid[pistonIDS.length / 2];
 
-        for(int i = 0; i < pistonIDS.length; i++) {
+        for (int i = 0; i < pistonIDS.length; i++) {
             doubleSolenoidPistons[i] = new DoubleSolenoid(pistonIDS[i][0], pistonIDS[i][1]);
         }
     }
 
     // Implementation: pistonsSet(kForward)
     public void pistonsSet(Value direction) {
-        for(DoubleSolenoid piston : doubleSolenoidPistons) {
+        for (DoubleSolenoid piston : doubleSolenoidPistons) {
             piston.set(direction);
         }
     }
 
     // Utilize these 2 methods as alternatives to the pistonsSet method
     public void extend() {
-        for(DoubleSolenoid piston : doubleSolenoidPistons) {
-            piston.set(Value.kForward);
+        for (DoubleSolenoid piston : doubleSolenoidPistons) {
+            pistonsSet(Value.kForward);
         }
     }
 
     public void retract() {
-        for(DoubleSolenoid piston : doubleSolenoidPistons) {
-            piston.set(Value.kReverse);
+        for (DoubleSolenoid piston : doubleSolenoidPistons) {
+            pistonsSet(Value.kReverse);
         }
     }
 }
