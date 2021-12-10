d2483d7ed14fc47124167c6ca63e460d8ea0eef8
Ben B <lerminator3@gmail.com>
2021-12-10 11:09:45 -0500

    changing feedforward calculation and usage

--- subsystems/FPIDSubsystem.java
+++ subsystems/FPIDSubsystem.java
@@ -1,10 +1,11 @@
 package org.frc5587.lib.subsystems;
 
-import org.frc5587.lib.pid.FPID;
+import org.frc5587.lib.pid.PID;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.controller.ArmFeedforward;
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.SpeedController;
 
@@ -23,7 +24,7 @@ public abstract class FPIDSubsystem extends PIDSubsystem {
         Velocity, Position
     }
 
-    public static class FeedforwardModel {
+    public static class FeedforwardModel extends ArmFeedforward {
         public final double kS;
         public final double kCos;
         public final double kV;
@@ -31,6 +32,7 @@ public abstract class FPIDSubsystem extends PIDSubsystem {
 
         // sets all given values for static, gravity, velocity, and acceleration gains.
         public FeedforwardModel(double kS, double kCos, double kV, double kA) {
+            super(kS, kCos, kV, kA);
             this.kS = kS;
             this.kCos = kCos;
             this.kV = kV;
@@ -40,23 +42,6 @@ public abstract class FPIDSubsystem extends PIDSubsystem {
         // use this for an elevator, or any FPIDSubsystem that does not require an angle gain.
         public FeedforwardModel(double kS, double kV, double kA) {
             this(kS, 0, kV, kA);
-        }
-
-        public double calculateAll(double positionRadians, double velocityRadPerSec, double accelRadPerSecSquared) {
-            return (
-                kS * Math.signum(velocityRadPerSec) 
-                + kCos * Math.cos(positionRadians)
-                + kV * velocityRadPerSec
-                + kA * accelRadPerSecSquared
-            );
-        }
-
-        public double calculateByAngle(double positionRadians, double velocityRadPerSec) {
-            return (
-                kS * Math.signum(velocityRadPerSec) 
-                + kCos * Math.cos(positionRadians)
-                + kV * velocityRadPerSec
-            );
         }
 
         public double calculateNoAngle(double velocityRadPerSec, double accelRadPerSecSquared) {
@@ -71,30 +56,27 @@ public abstract class FPIDSubsystem extends PIDSubsystem {
     public static class FPIDConstants {
         public double speedMultiplier, gearing;
         public int limitSwitchPort, encoderCPR;
-        public FPID fpid;
+        public boolean limitSwitchInverted;
+        public PID pid;
         public FeedforwardModel ff;
 
-        public FPIDConstants(double speedMultiplier, double gearing, int encoderCPR, int limitSwitchPort, FPID fpid, FeedforwardModel ff) {
+        public FPIDConstants(double speedMultiplier, double gearing, int encoderCPR, boolean limitSwitchInverted, int limitSwitchPort, PID pid, FeedforwardModel ff) {
             this.speedMultiplier = speedMultiplier;
             this.gearing = gearing;
             this.encoderCPR = encoderCPR;
             this.limitSwitchPort = limitSwitchPort;
-            this.fpid = fpid;
+            this.limitSwitchInverted = limitSwitchInverted;
+            this.pid = pid;
             this.ff = ff;
         }
     }
 
     // pass motors as an array of SpeedControllers, eg. WPI_TalonFX[] or CANSparkMax[]
     public FPIDSubsystem(FPIDConstants constants, SpeedController[] motors) {
-        super(new PIDController(constants.fpid.kP, constants.fpid.kI, constants.fpid.kD));
-        //disable PID control when starting
-        this.disable();
-        this.ffGain = constants.fpid.kF;
+        super(new PIDController(constants.pid.kP, constants.pid.kI, constants.pid.kD));
+        //enable PID control when starting
 
-        /* THE BELOW LINES WILL BE USED AFTER CHARACTERIZATION. DO NOT USE THEM NOW */
-
-        // this.enable();
-        // controller.enableFeedForward();
+        this.enable();
 
         this.constants = constants;
         limitSwitch = new DigitalInput(constants.limitSwitchPort);
@@ -131,13 +113,13 @@ public abstract class FPIDSubsystem extends PIDSubsystem {
     // calculates Feedforward using given position and velocity values (using an ArmFeedforward)
     // override this method if using a subsystem other than an arm.
     public double calcFeedForward(double position, double velocity) {
-        return constants.ff.calculateByAngle(position, velocity);
+        return constants.ff.calculate(position, velocity);
     }
 
     // calculates the Feedforward with a default value (using an ArmFeedforward)
     // override this method if using a subsystem other than an arm.
     public double calcFeedForward() {
-        return constants.ff.calculateByAngle(Math.toRadians(SmartDashboard.getNumber("Goto Position", 0)), 0);
+        return constants.ff.calculate(Math.toRadians(SmartDashboard.getNumber("Goto Position", 0)), 0);
     }
 
     // returns the Feedforward gain
@@ -162,8 +144,9 @@ public abstract class FPIDSubsystem extends PIDSubsystem {
         return getEncoderValue(EncoderValueType.Position) / constants.gearing / constants.encoderCPR;
     }
 
+    // returns the encoder position in degrees (based on the rotation)
     public double getPositionDegrees() {
-        return getPositionRotation() / 180;
+        return getPositionRotation() * 360;
     }
 
     public DigitalInput getLimitSwitch() {
@@ -171,20 +154,22 @@ public abstract class FPIDSubsystem extends PIDSubsystem {
     }
 
     public boolean getLimitSwitchValue() {
-        return !(limitSwitch.get());
+        // if constants tells us to invert the limit switch, return it as a negated version
+        return (constants.limitSwitchInverted ? !limitSwitch.get() : limitSwitch.get());
     }
     
     // uses PID output to move the mechanism
     @Override
     protected void useOutput(double output, double setpoint) {
         try {
-            moveByThrottle(output * ffGain);
+            moveByThrottle(output);
         }
         catch(NullPointerException e) {
             System.out.println("NullPointerException " + e + " from useOutput. \n A constant was likely not given by DriveConstants object");
         }
     }
 
+    // disables the subsystem without useing useOutput
     @Override
     public void disable() {
         this.m_enabled = false;
@@ -193,17 +178,18 @@ public abstract class FPIDSubsystem extends PIDSubsystem {
 
     @Override
     protected double getMeasurement() {
-        return getEncoderValue(EncoderValueType.Position);
+        return getPositionDegrees();
     }
 
     @Override
     public void periodic() {
-        // System.out.println(getPositionDegrees());
         refreshPID();
-        ffGain = calcFeedForward();
-        // double setpoint = constants.ff.calculate(Math.toRadians(getPositionDegrees()), getEncoderValue(EncoderValueType.Velocity));
-        // double output = controller.calculate(getMeasurement(), setpoint, getEncoderValue(EncoderValueType.Velocity));
-        // useOutput(output, setpoint);
+        // calculate a setpoint from the Feedforward model
+        double setpoint = constants.ff.calculate(Math.toRadians(getPositionDegrees()), getEncoderValue(EncoderValueType.Velocity));
+        // set the PIDController's setpoint to the above variable so it will automatically be used for output
+        setSetpoint(setpoint);
+        double output = getController().calculate(Math.toRadians(getPositionDegrees()));
+        useOutput(output, setpoint);
     }
 
     // sets encoders back to 0
