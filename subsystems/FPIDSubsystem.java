7a7c8fa54d062cb833db032a1deb2ec5789f4cc2
Max Gordon <tonald.drump2.0@gamil.com>
2022-01-22 23:52:11 -0500

    Documentation changes

--- subsystems/FPIDSubsystem.java
+++ subsystems/FPIDSubsystem.java
@@ -2,7 +2,6 @@ package org.frc5587.lib.subsystems;
 
 import org.frc5587.lib.controllers.FFController;
 import org.frc5587.lib.pid.PID;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
 import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.SpeedController;
@@ -23,18 +22,16 @@ public abstract class FPIDSubsystem extends ProfiledPIDSubsystem {
         public TrapezoidProfile.Constraints constraints;
 
         public FPIDConstants(
-        double speedMultiplier, 
-        double gearing, 
-        double[] softLimits,
-        int zeroOffset, 
-        int encoderCPR, 
-        int switchPort, 
-        boolean switchInverted, 
-        PID pid, 
-        FFController ff, 
-        TrapezoidProfile.Constraints constraints
-        ) 
-        {
+                double speedMultiplier,
+                double gearing,
+                double[] softLimits,
+                int zeroOffset,
+                int encoderCPR,
+                int switchPort,
+                boolean switchInverted,
+                PID pid,
+                FFController ff,
+                TrapezoidProfile.Constraints constraints) {
             this.speedMultiplier = speedMultiplier;
             this.gearing = gearing;
             this.softLimits = softLimits;
@@ -48,21 +45,17 @@ public abstract class FPIDSubsystem extends ProfiledPIDSubsystem {
         }
     }
 
-    /** 
-    * Pass motors as a SpeedController, so they can be passed as one motor,
-    * or made into a SpeedControllerGroup if there are multiple.
-    */
+    /**
+     * Pass motors as a SpeedController, so they can be passed as one motor,
+     * or made into a SpeedControllerGroup if there are multiple.
+     */
     public FPIDSubsystem(FPIDConstants constants, SpeedController motorGroup) {
         super(
-            new ProfiledPIDController(
-                constants.pid.kP, 
-                constants.pid.kI, 
-                constants.pid.kD,
-                constants.constraints
-            )
-        );
-        
-        // this.enable();
+                new ProfiledPIDController(
+                        constants.pid.kP,
+                        constants.pid.kI,
+                        constants.pid.kD,
+                        constants.constraints));
         this.constants = constants;
         this.motorGroup = motorGroup;
 
@@ -70,19 +63,21 @@ public abstract class FPIDSubsystem extends ProfiledPIDSubsystem {
     }
 
     /**
-    * Have the implementing class configure the motors 
-    */
+     * Have the implementing class configure the motors
+     */
     public abstract void configureMotors();
 
-    /**
-    * the implementing class also needs to get and set encoder values, as we don't know the encoder type
-    */
+    // * The implementing class also needs to get and set encoder values, as we don't
+
     /** @return the encoder's position */
     public abstract double getEncoderPosition();
+
     /** @return the encoder's velocity */
     public abstract double getEncoderVelocity();
+
     /** @param position the position to set the encoder to */
     public abstract void setEncoderPosition(double position);
+
     /**
      * @param rotations
      * @return the measurement used by the subsystem, converted from rotations
@@ -93,24 +88,27 @@ public abstract class FPIDSubsystem extends ProfiledPIDSubsystem {
     public abstract void periodic();
 
     /**
-    * Sets the motors to a percent output
-    * @param value the percent output to set the motors to (a double -1 to 1)
-    */
+     * Sets the motors to a percent output
+     * 
+     * @param value the percent output to set the motors to (a double -1 to 1)
+     */
     public void set(double throttle) {
         motorGroup.set(throttle);
         System.out.println("set the throttle to " + throttle);
     }
 
     /**
-    * Moves the motors by voltage instead of percent output
-    * @param voltage the voltage to set the motors to (a double -12 to 12)
-    */
+     * Moves the motors by voltage instead of percent output
+     * 
+     * @param voltage the voltage to set the motors to (a double -12 to 12)
+     */
     public void setVoltage(double voltage) {
         motorGroup.setVoltage(voltage);
     }
 
     /**
-     * @return the subsystem's measurement in the units used by the implementing subsystem.
+     * @return the subsystem's measurement in the units used by the implementing
+     *         subsystem.
      */
     @Override
     public double getMeasurement() {
@@ -118,67 +116,68 @@ public abstract class FPIDSubsystem extends ProfiledPIDSubsystem {
     }
 
     /**
-    * Uses PID output to move the mechanism
-    * <p> Make sure that the implementing class overrides this, because all
-    * FPIDSubsystems will use output differently.
-    */
+     * Uses PID output to move the mechanism
+     * <p>
+     * Make sure that the implementing class overrides this, because all
+     * FPIDSubsystems will use output differently.
+     */
     @Override
     protected abstract void useOutput(double output, TrapezoidProfile.State profileState);
 
-    /** 
-    * Disables PID without using useOutput
-    */
+    /**
+     * Disables PID without using useOutput
+     */
     @Override
     public void disable() {
         this.m_enabled = false;
         try {
             motorGroup.set(0);
-        }
-        catch(NullPointerException e) {
+        } catch (NullPointerException e) {
             System.out.println(e + " Could not get motor group.");
         }
     }
 
     /**
-    * @param value value to divide by the gearing set in constants
-    */
+     * @param value value to divide by the gearing set in constants
+     */
     public double applyGearing(double value) {
         return value / constants.gearing;
     }
 
     /**
-    * @param value value to divide by the encoder counts per revolution set in constants
-    */
+     * @param value value to divide by the encoder counts per revolution set in
+     *              constants
+     */
     public double applyCPR(double value) {
         return value / constants.encoderCPR;
     }
 
     /**
-    * @return the position of the subsystem in rotations,
-    * accounting for gearing and encoder counts per revolution.
-    */
+     * @return the position of the subsystem in rotations,
+     *         accounting for gearing and encoder counts per revolution.
+     */
     public double getRotations() {
         return applyCPR(applyGearing(getEncoderPosition()));
     }
 
     /**
-    * @return the velocity of the subsystem (in RPS),
-    * accounting for encoderCPR and gearing.
-    */
+     * @return the velocity of the subsystem (in RPS),
+     *         accounting for encoderCPR and gearing.
+     */
     public double getRotationsPerSecond() {
         return applyCPR(applyGearing(getEncoderVelocity()));
     }
 
-    /** 
-    * Sets encoders back to the zero offset of the subsystem.
-    */
+    /**
+     * Sets encoders back to the zero offset of the subsystem.
+     */
     public void resetEncoders() {
         setEncoderPosition(constants.zeroOffset);
     }
 
     /**
-    * Stops the SpeedControllerGroup
-    */
+     * Stops the SpeedControllerGroup
+     */
     public void stop() {
         motorGroup.set(0);
     }
