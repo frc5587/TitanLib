0d4cc5bed037b97f6f260ff4faa257ea9232ebae
Ben B <lerminator3@gmail.com>
2022-03-30 23:26:09 -0400

    allow support for pathplanner paths

--- auto/AutoPath.java
+++ auto/AutoPath.java
@@ -29,4 +29,38 @@ public class AutoPath {
             throw new RuntimeException("Could not find trajectory file:   " + path);
         }
     }
+
+    /**
+     * Used to load the Pathweaver trajectories into the {@link RamseteCommandWrapper}. 
+     * Note: this expects the file to have the extension `.wpilib.json`
+     * 
+     * @param fileName name of file (everything before the extension)
+     * @param pathPlannerDirectory whether the path is in the pathplanner directory. If true, the path will
+     *                             be searched for in 'src/main/deploy/pathplanner/generatedJSON'. Otherwise
+     *                             it will use the filepath 'src/main/deploy/paths/output'
+     */
+    public AutoPath(String fileName, boolean pathPlannerDirectory) {
+        if(!pathPlannerDirectory) {
+            filePath = "paths/output/" + fileName + ".wpilib.json";
+            path = Filesystem.getDeployDirectory().toPath().resolve(filePath);
+
+            try {
+                this.trajectory = TrajectoryUtil.fromPathweaverJson(path);
+            } catch (IOException e) {
+                // Fail hard so people know quickly when something is wrong instead of a NullPointerException down the road
+                throw new RuntimeException("Could not find trajectory file:   " + path);
+            }
+        }
+        else {
+            filePath = "pathplanner/generatedJSON/" + fileName + ".wpilib.json";
+            path = Filesystem.getDeployDirectory().toPath().resolve(filePath);
+
+            try {
+                this.trajectory = TrajectoryUtil.fromPathweaverJson(path);
+            } catch (IOException e) {
+                // Fail hard so people know quickly when something is wrong instead of a NullPointerException down the road
+                throw new RuntimeException("Could not find trajectory file:   " + path);
+            }
+        }
+    }
 }
