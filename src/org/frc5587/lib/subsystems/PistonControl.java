b3fb983bab48de2e6171fd10e8773583dc892799
Hazel Belmont <hparkerbelmont@gmail.com>
2022-12-01 16:36:05 -0500

    yay!

--- src/org/frc5587/lib/subsystems/PistonControl.java
+++ src/org/frc5587/lib/subsystems/PistonControl.java
@@ -1,7 +1,6 @@
 package org.frc5587.lib.subsystems;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
