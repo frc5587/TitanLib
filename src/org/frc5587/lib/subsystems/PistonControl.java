7703296cffb505920922c688fe3f9a2be547b138
Hazel Belmont <hparkerbelmont@gmail.com>
2022-12-01 16:13:46 -0500

    oops!!!

--- src/org/frc5587/lib/subsystems/PistonControl.java
+++ src/org/frc5587/lib/subsystems/PistonControl.java
@@ -3,34 +3,33 @@ package org.frc5587.lib.subsystems;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
-import static edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public abstract class PistonControl extends SubsystemBase {
     private DoubleSolenoid[] doubleSolenoidPistons;
 
-/**
- * Creates new doublesolenoids for the amount of piston ports specified in
- * constants.java
- */
-public PistonControl(DoubleSolenoid[] solenoids) {
-    doubleSolenoidPistons = solenoids;
-}
-
-// Implementation: pistonsSet(kForward)
-public void pistonsSet(Value direction) {
-    for (DoubleSolenoid piston : doubleSolenoidPistons) {
-        piston.set(direction);
+    /**
+     * Creates new doublesolenoids for the amount of piston ports specified in
+     * constants.java
+     */
+    public PistonControl(DoubleSolenoid[] solenoids) {
+        doubleSolenoidPistons = solenoids;
     }
-}
 
-// Utilize these 2 methods as alternatives to the pistonsSet method
-public void extend() {
-    pistonsSet(Value.kForward);
-}
+    // Implementation: pistonsSet(kForward)
+    public void pistonsSet(Value direction) {
+        for (DoubleSolenoid piston : doubleSolenoidPistons) {
+            piston.set(direction);
+        }
+    }
 
-public void retract() {
-    pistonsSet(Value.kReverse);
-}
+    // Utilize these 2 methods as alternatives to the pistonsSet method
+    public void extend() {
+        pistonsSet(Value.kForward);
+    }
 
+    public void retract() {
+        pistonsSet(Value.kReverse);
+    }
 }
