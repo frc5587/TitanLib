638392e36b239cb2a273322cdaab896446aa9344
Hazel Belmont <hparkerbelmont@gmail.com>
2024-01-14 22:36:42 -0500

    initialize some things in swervebase and add javadoc

--- src/org/frc5587/lib/subsystems/SwerveBase.java
+++ src/org/frc5587/lib/subsystems/SwerveBase.java
@@ -32,9 +32,9 @@ public class SwerveBase extends SubsystemBase {
         }
     }
 
-    public SwerveConstants constants;
-    public SwerveModuleBase[] mSwerveMods;
-    public GyroBase gyro;
+    public final SwerveConstants constants;
+    public final SwerveModuleBase[] swerveModules;
+    public final GyroBase gyro;
     
     public SwerveDriveOdometry odometry;
     public SwerveDrivePoseEstimator poseEstimator;
@@ -44,7 +44,9 @@ public class SwerveBase extends SubsystemBase {
     public Field2d field = new Field2d();
     public Double lockedHeading = null;
 
-    public SwerveBase() {
+    public SwerveBase(SwerveConstants constants, SwerveModuleBase[] swerveModules) {
+        this.constants = constants;
+        this.swerveModules = swerveModules;
         this.gyro = new TitanNavX(constants.gyroInverted);
         this.kinematics = constants.kinematics;
 
@@ -57,6 +59,23 @@ public class SwerveBase extends SubsystemBase {
         resetModulesToAbsolute();
     }
 
+    /**
+     * @param translation the amount of x-and-y-change to move the robot by - note that the measurement
+     *                    given to this translation may not be the actual amount that the robot moves;
+     *                    this is more like a proportional output (i.e. if the translation is 3 meters
+     *                    by 4 meters, the robot may move 0.5 meters by 0.7 meters).
+     * @param rotation the amount of rotation to move the robot by - note that the measurement given to
+     *                 this rotation may not be the actual amount that the robot moves; this is more like
+     *                 a proportional output (i.e. if the rotation is 50 degrees, the robot may move 12.5
+     *                 degrees).
+     * @param fieldRelative whether the input should be taken as field relative. For example: if true, a
+     *                      positive x will move the robot away from the driver no matter the rotation of
+     *                      the robot, and if false, a positive x will move the robot towards its own front
+     *                      no matter the rotation of the robot, and so on.
+     * @param isOpenLoop whether to use open-loop velocity control for the drive motors. If false, the velocity
+     *                   of the robot may not be the same as the velocity calculated in the chassis speeds, but
+     *                   it will probably be proportional.
+     */
     public void drive(Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop) {
         SwerveModuleState[] swerveModuleStates =
             kinematics.toSwerveModuleStates(
@@ -70,41 +89,60 @@ public class SwerveBase extends SubsystemBase {
                         translation.getY(), 
                         rotation));
 
-        SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, constants.maxSpeedMetersPerSecond);
-        
-        for(SwerveModuleBase mod : mSwerveMods){
-            mod.setDesiredState(swerveModuleStates[mod.moduleNumber], isOpenLoop);
-        }
-
-        // setModuleStates(swerveModuleStates);
+        setModuleStates(swerveModuleStates, isOpenLoop);
     }    
 
-    /* Used by SwerveControllerCommand in Auto */
-    public void setModuleStates(SwerveModuleState[] desiredStates) {
+    /**
+     * Sets the desired states of each module.
+     */
+    public void setModuleStates(SwerveModuleState[] desiredStates, boolean isOpenLoop) {
         SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, constants.maxSpeedMetersPerSecond);
         
-        for(SwerveModuleBase mod : mSwerveMods){
-            mod.setDesiredState(desiredStates[mod.moduleNumber], false);
+        for(SwerveModuleBase mod : swerveModules){
+            mod.setDesiredState(desiredStates[mod.moduleNumber], isOpenLoop);
         }
     }
 
+    /**
+     * Sets the desired states of each module and assumes closed-loop control.
+     */
+    public void setModuleStates(SwerveModuleState[] desiredStates) {
+        setModuleStates(desiredStates, false);
+    }
+
+    /**
+     * Sets the module states based on chassis speeds.
+     */
     public void setChassisSpeeds(ChassisSpeeds speeds) {
         speeds = new ChassisSpeeds(-speeds.vxMetersPerSecond, -speeds.vyMetersPerSecond, speeds.omegaRadiansPerSecond);
         setModuleStates(kinematics.toSwerveModuleStates(speeds));
     }
 
+    /**
+     * @return the robot pose from the pose estimator.
+     */
     public Pose2d getPose() {
         return getEstimatedPose(); 
     }
 
+    /**
+     * @return the robot pose from odometry
+     */
     public Pose2d getOdometryPose() {
         return odometry.getPoseMeters();
     }
 
+    /**
+     * @return the robot pose from the pose estimator.
+     */
     public Pose2d getEstimatedPose() {
         return poseEstimator.getEstimatedPosition();
     }
 
+    /**
+     * Resets odometry, gyro yaw, and pose estimator to the given pose.
+     * @param pose the position to reset the odometry to.
+     */
     public void resetOdometry(Pose2d pose) {
         gyro.zeroYaw();
         gyro.setYawZeroOffset(pose.getRotation());
@@ -113,55 +151,89 @@ public class SwerveBase extends SubsystemBase {
         poseEstimator.resetPosition(getYaw(), getModulePositions(), pose);
     }
 
+    /**
+     * Resets odometry, gyro yaw, and pose estimator to an empty Pose2d
+     * (i.e. 0 meters, 0 meters, 0 degrees)
+     */
     public void zeroOdometry() {
         resetOdometry(new Pose2d());
     }
 
+    /**
+     * @return an array of SwerveModuleStates that represent the angle and velocity
+     * of each module
+     */
     public SwerveModuleState[] getModuleStates(){
         SwerveModuleState[] states = new SwerveModuleState[4];
-        for(SwerveModuleBase mod : mSwerveMods){
+        for(SwerveModuleBase mod : swerveModules){
             states[mod.moduleNumber] = mod.getState();
         }
         return states;
     }
 
+    /**
+     * @return an array of SwerveModulePositions that represent the angle and drive motor
+     * position of each module
+     */
     public SwerveModulePosition[] getModulePositions(){
         SwerveModulePosition[] positions = new SwerveModulePosition[4];
-        for(SwerveModuleBase mod : mSwerveMods){
+        for(SwerveModuleBase mod : swerveModules){
             positions[mod.moduleNumber] = mod.getPosition();
         }
         return positions;
     }
 
+    /**
+     * Sets the zero position of the gyro's yaw to the current yaw position
+     */
     public void zeroGyro(){
         lockedHeading = null;
         gyro.zeroYaw();
     }
 
+    /**
+     * @return the gyro roll.
+     */
     public Rotation2d getRoll() {
         return gyro.getRoll();
     }
 
+    /**
+     * @return the gyro pitch.
+     */
     public Rotation2d getPitch() {
         return gyro.getPitch();
     }
 
+    /**
+     * @return the gyro yaw.
+     */
     public Rotation2d getYaw() {
         return gyro.getYaw();
     }
 
+    /**
+     * @return the yaw from odometry, notably does not include any gyro information
+     */
     public Rotation2d getYawFromOdom() {
         return odometry.getPoseMeters().getRotation();
     }
 
+    /**
+     * Sets all angle motor encoder positions to the position of the absolute encoder
+     */
     public void resetModulesToAbsolute(){
-        for(SwerveModuleBase mod : mSwerveMods){
+        for(SwerveModuleBase mod : swerveModules){
             mod.resetToAbsolute();
         }
     }
 
+    /**
+     * Sets the speed of all drive motors to 0 and sets the angle of all modules to their
+     * current angle
+     */
     public void stop() {
-        for (SwerveModuleBase mod : mSwerveMods) {
+        for (SwerveModuleBase mod : swerveModules) {
             mod.stop();
         }
     }
@@ -169,9 +241,8 @@ public class SwerveBase extends SubsystemBase {
     @Override
     public void periodic() {
         odometry.update(getYaw(), getModulePositions());
-        poseEstimator.updateWithTime(Timer.getFPGATimestamp(), getYaw(), getModulePositions()); // ! If this is wrong, its probably a problem with getYaw()
+        poseEstimator.updateWithTime(Timer.getFPGATimestamp(), getYaw(), getModulePositions());
         poseHistory.addSample(Timer.getFPGATimestamp(), getPose());
         field.setRobotPose(getPose());
-        getModuleStates();
     }
 }
--- src/org/frc5587/lib/subsystems/SwerveModuleBase.java
+++ src/org/frc5587/lib/subsystems/SwerveModuleBase.java
@@ -7,21 +7,10 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 
 public abstract class SwerveModuleBase {
-    public int moduleNumber;
-    protected Rotation2d angleOffset;
-    protected Rotation2d lastAngle = new Rotation2d();
-
-    protected MotorController angleMotor;
-    protected MotorController driveMotor;
-
-    protected SimpleMotorFeedforward feedforward;
-
     public static class SwerveModuleConstants {
-        public final boolean absoluteEncoderInverted;
         public final Rotation2d angleOffset;
         public final double wheelCircumferenceMeters;
         public final double maxSpeedMetersPerSecond;
-        public final SimpleMotorFeedforward feedforward;
         public final double angleMotorEncoderCPR;
         public final double driveMotorEncoderCPR;
         public final double angleMotorGearRatio;
@@ -29,33 +18,56 @@ public abstract class SwerveModuleBase {
 
         @Deprecated()
         /**
-         * Note: @deprecated the field angleOffset is deprecated and should not be used.
-         * Instead, use the native firmware-zero feature of the absolute encoder (i.e. zero
-         * the encoder in Phoenix Tuner.
-         * @param absoluteEncoderInverted
-         * @param angleOffset
-         * @param wheelCircumferenceMeters
-         * @param maxSpeedMetersPerSecond
-         * @param feedforward
+         * @param angleOffset the offset to subtract from the absolute encoder reading.
+         *                    Note: @deprecated the field angleOffset is deprecated and should not be used.
+         *                    Instead, use the native firmware-zero feature of the absolute encoder (i.e. zero
+         *                    the encoder in Phoenix Tuner.
+         * @param wheelCircumferenceMeters the wheel circumference in meters.
+         * @param maxSpeedMetersPerSecond the maximum desired speed of the drive motors in m/s.
+         * @param angleMotorEncoderCPR the counts per revolution of the angle motor's encoder.
+         *                             SPARK MAXes already handle this value, so this value would
+         *                             be 1 for swerves using SPARK MAX. CTRE motors with Talon FX
+         *                             (Falcon 500, Kraken X60) have a CPR of 2048.
+         * @param driveMotorEncoderCPR the counts per revolution of the drive motor's encoder.
+         *                             SPARK MAXes already handle this value, so this value would
+         *                             be 1 for swerves using SPARK MAX. CTRE motors with Talon FX
+         *                             (Falcon 500, Kraken X60) have a CPR of 2048.
+         * @param angleMotorGearRatio the gear ratio between the angle motor's output shaft and the
+         *                            wheel's rotation. For SDS Mk4i modules, this would be 21.429.
+         * @param driveMotorGearRatio the gear ration between the drive motor's output shaft and the
+         *                            wheel's rotation. For L1 SDS Mk4i modules, this would be 8.14.
+         *                            L2 SDS Mk4i modules, this would be 6.75.
          */
-        public SwerveModuleConstants(boolean absoluteEncoderInverted, Rotation2d angleOffset, double wheelCircumferenceMeters, double maxSpeedMetersPerSecond, SimpleMotorFeedforward feedforward, double angleMotorEncoderCPR, double driveMotorEncoderCPR, double angleMotorGearRatio, double driveMotorGearRatio) {
-            this.absoluteEncoderInverted = absoluteEncoderInverted;
+        public SwerveModuleConstants(Rotation2d angleOffset, double wheelCircumferenceMeters, double maxSpeedMetersPerSecond, double angleMotorEncoderCPR, double driveMotorEncoderCPR, double angleMotorGearRatio, double driveMotorGearRatio) {
             this.angleOffset = angleOffset;
             this.wheelCircumferenceMeters = wheelCircumferenceMeters;
             this.maxSpeedMetersPerSecond = maxSpeedMetersPerSecond;
-            this.feedforward = feedforward;
             this.angleMotorEncoderCPR = angleMotorEncoderCPR;
             this.driveMotorEncoderCPR = driveMotorEncoderCPR;
             this.angleMotorGearRatio = angleMotorGearRatio;
             this.driveMotorGearRatio = driveMotorGearRatio;
         }
 
-        public SwerveModuleConstants(boolean absoluteEncoderInverted, double wheelCircumferenceMeters, double maxSpeedMetersPerSecond, SimpleMotorFeedforward feedforward, double angleMotorEncoderCPR, double driveMotorEncoderCPR, double angleMotorGearRatio, double driveMotorGearRatio) {
-            this.absoluteEncoderInverted = absoluteEncoderInverted;
+        /**
+         * @param wheelCircumferenceMeters the wheel circumference in meters.
+         * @param maxSpeedMetersPerSecond the maximum desired speed of the drive motors in m/s.
+         * @param angleMotorEncoderCPR the counts per revolution of the angle motor's encoder.
+         *                             SPARK MAXes already handle this value, so this value would
+         *                             be 1 for swerves using SPARK MAX. CTRE motors with Talon FX
+         *                             (Falcon 500, Kraken X60) have a CPR of 2048.
+         * @param driveMotorEncoderCPR the counts per revolution of the drive motor's encoder.
+         *                             SPARK MAXes already handle this value, so this value would
+         *                             be 1 for swerves using SPARK MAX. CTRE motors with Talon FX
+         *                             (Falcon 500, Kraken X60) have a CPR of 2048.
+         * @param angleMotorGearRatio the gear ratio between the angle motor's output shaft and the
+         *                            wheel's rotation. For SDS Mk4i modules, this would be 21.429.
+         * @param driveMotorGearRatio the gear ration between the drive motor's output shaft and the
+         *                            wheel's rotation. For L2 SDS Mk4i modules, this would be 6.75.
+         */
+        public SwerveModuleConstants(double wheelCircumferenceMeters, double maxSpeedMetersPerSecond, double angleMotorEncoderCPR, double driveMotorEncoderCPR, double angleMotorGearRatio, double driveMotorGearRatio) {
             this.angleOffset = new Rotation2d();
             this.wheelCircumferenceMeters = wheelCircumferenceMeters;
             this.maxSpeedMetersPerSecond = maxSpeedMetersPerSecond;
-            this.feedforward = feedforward;
             this.angleMotorEncoderCPR = angleMotorEncoderCPR;
             this.driveMotorEncoderCPR = driveMotorEncoderCPR;
             this.angleMotorGearRatio = angleMotorGearRatio;
@@ -63,9 +75,14 @@ public abstract class SwerveModuleBase {
         }
     }
 
+    public int moduleNumber;
+    protected Rotation2d angleOffset;
+    protected Rotation2d lastAngle = new Rotation2d();
+    protected MotorController angleMotor;
+    protected MotorController driveMotor;
     protected SwerveModuleConstants moduleConstants;
 
-    public SwerveModuleBase(int moduleNumber, SwerveModuleConstants moduleConstants) {
+    public SwerveModuleBase(int moduleNumber, SwerveModuleConstants moduleConstants, MotorController angleMotor, MotorController driveMotor) {
         this.moduleConstants = moduleConstants;
         this.moduleNumber = moduleNumber;
         this.angleOffset = moduleConstants.angleOffset;
@@ -83,14 +100,31 @@ public abstract class SwerveModuleBase {
         lastAngle = getState().angle;
     }
 
+    /**
+     * Sets the entire module (angle motor and drive motor) to the desired state.
+     * @param desiredState the SwerveModuleState to set the module to.
+     * @param isOpenLoop whether to use open loop velocity control for the drive motor.
+     *                   If this is true, the resulting velocity of the drive motor should
+     *                   exactly match the supplied velocity from the SwerveModuleState (if
+     *                   all control gains have been set properly).
+     */
     public void setDesiredState(SwerveModuleState desiredState, boolean isOpenLoop) {
         /* This is a custom optimize function, since default WPILib optimize assumes continuous controller which CTRE and Rev onboard is not */
         desiredState = optimizeState(desiredState, getState().angle); 
         setAngle(desiredState);
-        setSpeed(desiredState, isOpenLoop);
+        setDriveSpeed(desiredState, isOpenLoop);
     }
 
-    private void setSpeed(SwerveModuleState desiredState, boolean isOpenLoop) {
+    /**
+     * Sets the speed of the drive motor based on the desired state.
+     * @param desiredState the SwerveModuleState to which supplies the velocity to set the
+     *                     drive motor to.
+     * @param isOpenLoop whether to use open loop velocity control for the drive motor.
+     *                   If this is true, the resulting velocity of the drive motor should
+     *                   exactly match the supplied velocity from the SwerveModuleState (if
+     *                   all control gains have been set properly).
+     */
+    public void setDriveSpeed(SwerveModuleState desiredState, boolean isOpenLoop) {
         if(isOpenLoop){
             double percentOutput = desiredState.speedMetersPerSecond / moduleConstants.maxSpeedMetersPerSecond;
             driveMotor.set(percentOutput);
@@ -100,14 +134,56 @@ public abstract class SwerveModuleBase {
         }
     }
 
+    /**
+     * Sets the speed of the drive motor.
+     * @param desiredState the velocity to set the drive motor to.
+     * @param isOpenLoop whether to use open loop velocity control for the drive motor.
+     *                   If this is true, the resulting velocity of the drive motor should
+     *                   exactly match the supplied velocity from the SwerveModuleState (if
+     *                   all control gains have been set properly). Otherwise, it will be
+     *                   used as a percentage of the max speed.
+     */
+    public void setDriveSpeed(double speedMetersPerSecond, boolean isOpenLoop) {
+        if(isOpenLoop){
+            double percentOutput = speedMetersPerSecond / moduleConstants.maxSpeedMetersPerSecond;
+            driveMotor.set(percentOutput);
+        }
+        else {
+            setDriveMotorVelocity(speedMetersPerSecond);
+        }
+    }
+
+    /**
+     * Sets the position of the angle motor to the angle given by the desired state.
+     * There is some optimization to decrease wheel jittering by preventing the module
+     * from moving if the drive motor speed is less than 5% of the max speed.
+     * @param desiredState the state that includes the drive motor's speed (necessary
+     *                     for the optimization feature) and the desired angle.
+     */
     public void setAngle(SwerveModuleState desiredState) {
-        Rotation2d angle = (Math.abs(desiredState.speedMetersPerSecond) <= (moduleConstants.maxSpeedMetersPerSecond * 0.05)) ? lastAngle : desiredState.angle; //Prevent rotating module if speed is less then 5%. Prevents Jittering.
+        Rotation2d optimizedAngle = (Math.abs(desiredState.speedMetersPerSecond) <= (moduleConstants.maxSpeedMetersPerSecond * 0.05)) ? lastAngle : desiredState.angle; //Prevent rotating module if speed is less then 5%. Prevents Jittering.
         
+        setAngleMotorPosition(optimizedAngle);
+        lastAngle = optimizedAngle;
+    }
+
+    /**
+     * Sets the position of the angle motor to the given angle. Notably, this overload does
+     * not include the speed optimization feature as there is no drive motor speed given to
+     * the method.
+     * @param angle the desired angle of the swerve module.
+     */
+    public void setAngle(Rotation2d angle) {        
         setAngleMotorPosition(angle);
         lastAngle = angle;
     }
 
-    private Rotation2d getAngle(){
+    /**
+     * @return the rotational position of the swerve module's wheel. In other words,
+     *         the angle motor's position divided by its encoder CPR and its gear ratio
+     *         relative to the wheel's rotation.
+     */
+    public Rotation2d getAngle(){
         return Rotation2d.fromDegrees((360 * getAngleMotorEncoderPosition() / moduleConstants.angleMotorEncoderCPR) / moduleConstants.angleMotorGearRatio);
     }
 
